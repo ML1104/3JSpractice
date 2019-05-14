7e888a966155efaae9a0c14c8dfe2be15c31bc78
ML1104 <miloske1104@gmail.com>
2019-05-14 21:09:32 +0200

    first commit

---
+++ js/effects.js
@@ -0,0 +1,7 @@
+var startButton = document.getElementById('entry-text');
+
+function explode() {
+    startButton.style.fontSize = '400px';
+    startButton.style.opacity = '0';
+    startButton.style.transition = '3s';
+}
